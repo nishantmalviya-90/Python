459976efcd41a0e553981c43f4c870c5e3306926
@nishantmalviya-90 <nishantmalviya69@gmail.com>
2024-07-23 20:36:35 +0530

    python

---
+++ runmodule.py
@@ -0,0 +1,5 @@
+# syntax from module_name import fun_name,variable_name,class_name
+from module import add as a #as keyword is used for making aliasing of any class or function
+x=int(input("enter any no.")) 
+y=int(input("enter any no.")) 
+print(a(x,y))
